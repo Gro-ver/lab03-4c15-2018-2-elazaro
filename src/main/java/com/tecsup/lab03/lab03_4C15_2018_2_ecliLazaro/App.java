34f13d33f3c5e864635c85b0ce5115c5ef60b4f2
Gro-ver <grover.lazaro@tecsup.edu.pe>
2018-09-07 11:35:33 -0500

    branch "sprint2"

--- src/main/java/com/tecsup/lab03/lab03_4C15_2018_2_ecliLazaro/App.java
+++ src/main/java/com/tecsup/lab03/lab03_4C15_2018_2_ecliLazaro/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!  saludosss" );
+        System.out.println( "este proyecto es de la sprint2" );
     }
 }
